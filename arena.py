f785859c849a012201e4254ec9af810921d4ea00
gvnn <gvnnrulez@gmail.com>
2011-09-28 09:51:04 +1000

    added a label.. I'm trying to build a console terminal on the bottom

--- arena.py
+++ arena.py
@@ -8,13 +8,15 @@ import arenamodel
 def newgame(client):
     """returns the game scene"""
     scene = cocos.scene.Scene()
+    console = arenaview.ConsoleLayer()
     model = arenamodel.ArenaModel() # model
     ctrl = arenacontroller.ArenaController(model, client) # controller
-    view = arenaview.ArenaView(model, client) # view
+    view = arenaview.ArenaView(model, client, console) # view
     # set controller in model
     model.set_controller(ctrl)
     # add controller
-    scene.add(ctrl, z=0, name="controller")
-    # add view
-    scene.add(view, z=1, name="view")
+    scene.add(ctrl, z = 1, name = "controller")
+    # add view & console
+    scene.add(console, z = 3, name = "console")
+    scene.add(view, z = 2, name = "view")
     return scene
--- arenaview.py
+++ arenaview.py
@@ -1,42 +1,60 @@
 #!/usr/bin/env python
-import pyglet
 import cocos
+import cocos.actions as ac
 from cocos.director import director
+import pyglet
+import time
 
 class ArenaView(cocos.layer.Layer):
     """ View  (of the MVC pattern) """
     
-    def __init__(self, model, client):
+    messages = []
+    
+    def __init__(self, model, client, console):
         super(ArenaView,self).__init__()
         self.model = model
         self.client = client
+        self.consolelayer = console
         self.model.push_handlers(self.on_local_change_direction)
         self.client.push_handlers(self.on_remote_connected)
         self.client.push_handlers(self.on_remote_disconnected)
         self.client.push_handlers(self.on_remote_change_direction)
-        self.add(LogLayer())
+        self.schedule(self.update_console)
+        self.consolelayer.push_text("Welcome!")
         
+    def update_console(self, dt):
+        for msg in self.messages:
+            self.consolelayer.push_text(msg)
+            self.messages.remove(msg) #remove from the list
+
     def on_local_change_direction(self, direction):
         self.client.confobj.logger.debug("%s" % direction)
-        return True
         
     def on_remote_change_direction(self, clientid, direction):
         self.client.confobj.logger.debug("%s - %s" % (clientid, direction))
-        return True
         
     def on_remote_connected(self, clientid):
         self.client.confobj.logger.debug("%s" % clientid)
-        return True
+        self.messages.append("%d - %s \n ciao" % (time.time(), clientid))
         
     def on_remote_disconnected(self, clientid):
         self.client.confobj.logger.debug("%s" % clientid)
-        return True
 
-class LogLayer(cocos.layer.Layer):
+class ConsoleLayer(cocos.layer.Layer):
+    
+    labels = []
+    
+    def label(self):
+        return cocos.text.Label("", font_size = 11, font_name = "Courier New", color = (0, 255, 0, 255))
     
     def __init__(self):
-        super(LogLayer, self).__init__()
-        w,h = director.get_window_size()
-        
-    def draw(self):
-        super(LogLayer, self).draw()
+        super(ConsoleLayer, self).__init__()
+    
+    def push_text(self, text):
+        label = self.label()
+        label.element.text = text
+        labels.insert(label, 0)
+    
+    def draw_labels(self):
+        for i in range(3):
+            
